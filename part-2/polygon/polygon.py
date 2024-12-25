dd6930bb4d07cc7e7b50395141daa16a2e518000
Rafael Riedel <rafaelriedel@gmail.com>
2024-12-25 11:51:58 -0300

    Goal 2 refactor

--- part-2/polygon/polygon.py
+++ part-2/polygon/polygon.py
@@ -133,29 +133,50 @@ class Polygon:
 
 
 class Polygons:
-    def __init__(self, max_n, R):
-        if max_n < 3:
-            raise ValueError("max_n must be greater than 3")
+    def __init__(self, m, R):
+        if m < 3:
+            raise ValueError("m must be greater than 3")
 
-        self._max_n = max_n
+        self._m = m
         self._R = R
 
-        self._polygons = [Polygon(i, R) for i in range(3, self._max_n+1)]
 
     def __len__(self):
-        return self._max_n - 2
+        return self._m - 2
 
     def __repr__(self):
-        return f'Polygons(max_n={self._max_n}, R={self._R})'
+        return f'Polygons(m={self._m}, R={self._R})'
 
-    def __getitem__(self, s):
-        return self._polygons[s]
+    def __iter__(self):
+        return PolygonIterator(self._m, self._R)
 
     @property
     def max_efficiency_polygon(self):
-        sorted_polygons = sorted(self._polygons, key=lambda p: p.area / p.perimeter, reverse=True)
+        sorted_polygons = sorted(PolygonIterator(self._m, self._R),
+            key=lambda p: p.area / p.perimeter,
+            reverse=True)
         return sorted_polygons[0]
 
+
+class PolygonIterator:
+    def __init__(self, m, R):
+        if m < 3:
+            raise ValueError("m must be greater than 3")
+
+        self._m = m
+        self._R = R
+        self._i = 3
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._i > self._m:
+            raise StopIteration
+        else:
+            result = Polygon(self._i, self._R)
+            self._i += 1
+            return result
 
 if __name__ == '__main__':
     p = Polygons(10, 1)
